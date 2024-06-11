e1039b43a3cf0944860e3738578f03164b4a7f26
Alessandra Gherardelli <AlexGherardelli@users.noreply.github.com>
2024-06-11 13:57:15 +0200

    Trying to add STATA labels to dataset

--- data_bridges_utils/load_stata.py
+++ data_bridges_utils/load_stata.py
@@ -1,27 +1,41 @@
 import stata_setup
+from data_bridges_utils.labels import get_column_labels, get_value_labels
 
-def load_stata(df, stata_path =  "C:/Program Files/Stata18", stata_version = "se"):
+def load_stata(df, stata_path="C:/Program Files/Stata18", stata_version="se", variable_labels=None, value_labels=None):
     stata_setup.config(stata_path, stata_version)
-    from sfi import Data, Macro,  SFIToolkit, Frame, Datetime as dt
+    from sfi import Data, Macro, SFIToolkit, Frame, Datetime as dt
+
     """
     Loads a Pandas DataFrame into a Stata data file format.
-    
+
     Args:
         df (pandas.DataFrame): The DataFrame to be loaded into Stata format.
-    
+        stata_path (str, optional): The path to the Stata installation. Defaults to "C:/Program Files/Stata18".
+        stata_version (str, optional): The Stata version. Defaults to "se".
+        variable_labels (dict, optional): A dictionary containing variable labels. Defaults to None.
+        value_labels (dict, optional): A dictionary containing value labels. Defaults to None.
+
     Returns:
         pandas.DataFrame: The original DataFrame.
     """
     colnames = df.columns
     Data.setObsTotal(len(df))
+
     for i in range(len(colnames)):
         dtype = df.dtypes[i].name
-        print(dtype)
-        # make a valid Stata variable name
         varname = SFIToolkit.makeVarName(colnames[i], retainCase=True)
-        print(colnames[i])
-        # varname = colnames[i]
         varval = df[colnames[i]].values.tolist()
+
+        if variable_labels and varname in variable_labels:
+            Data.setVarLabel(varname, variable_labels[varname])
+
+        if value_labels and varname in value_labels:
+            value_label_name = f"{varname}_value_label"
+            ValueLabel.createLabel(value_label_name)
+            for value, label in value_labels[varname].items():
+                ValueLabel.setLabelValue(value_label_name, value, label)
+            ValueLabel.setVarValueLabel(varname, value_label_name)
+
         if dtype == "int64":
             Data.addVarInt(varname)
             Data.store(varname, None, varval)
@@ -37,10 +51,10 @@ def load_stata(df, stata_path =  "C:/Program Files/Stata18", stata_version = "se
             Data.store(varname, None, price_dt_py)
             Data.setVarFormat(varname, '%tdCCYY-NN-DD')
         else:
-            # all other types store as a string
             Data.addVarStr(varname, 1)
             s = [str(i) for i in varval]
             Data.store(varname, None, s)
+
     return df
 
 
